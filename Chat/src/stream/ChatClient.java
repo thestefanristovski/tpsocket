13476c6633475d373c8216b202e16a8dcd8ac6e6
thestefanristovski <stefan.ristovski1@gmail.com>
2020-10-21 09:36:30 +0200

    Removed unecessary files

--- Chat/src/stream/ChatClient.java
+++ Chat/src/stream/ChatClient.java
@@ -41,7 +41,6 @@ public class ChatClient {
             System.exit(1);
         }
 
-
         System.out.println("Welcome to Messenger! Please enter your username: ");
         ClientSendThread sendThread = new ClientSendThread(echoSocket);
         ClientReceiveThread receiveThread = new ClientReceiveThread(echoSocket);
